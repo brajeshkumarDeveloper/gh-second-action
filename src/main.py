4eb080d9a788ce9483060a0ba903e2e2d89d5950
brajeshkumarDeveloper <brajeshkewat8349@gmail.com>
2026-01-28 16:41:00 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def add_function(a,b):
+    return a+b;
+
+print(add_function(3,4));
+
+def subtract_function(a, b):
+    return a-b;
+
+print(add_function(3,4));
+print(subtract_function(4,3));
---
+++ tests/main_tests.py
@@ -0,0 +1,15 @@
+from src.main import add_function, subtract_function
+
+
+def test_add_function():
+    assert add_function(10,20) == 30
+    assert add_function(0, 0) == 0
+    assert add_function(0, 1) == 1
+
+
+def test_subtraction_function():
+    assert subtract_function(10,20) == -10
+    assert subtract_function(0, 0) == 0
+    assert subtract_function(0, 1) == -1
+
+test_add_function()
