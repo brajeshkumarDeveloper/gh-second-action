ead290bcaee4f6b5ed67ade5cabb835a4d3709e7
brajeshkumarDeveloper <brajeshkewat8349@gmail.com>
2026-01-28 16:51:16 +0530

    fixed the lint issue

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-def add_function(a,b):
-    return a+b;
+def add_function(a, b):
+    return a + b;
 
 print(add_function(3,4));
 
 def subtract_function(a, b):
-    return a-b;
+    return a-b
 
-print(add_function(3,4));
-print(subtract_function(4,3));
+
+print(add_function(3,4))
+print(subtract_function(4,3))
